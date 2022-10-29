df848749c12ecb84f323b2162dacba881d618041
Łukasz Stępień <lukstep@macbook-1.home>
2022-10-29 21:54:37 +0200

    Lualine

---
+++ nvim/config/lualine.lua
@@ -0,0 +1,67 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+    vim.notify("Lualine not found!")   
+    return
+end
+
+local diagnostics = {
+    "diagnostics",
+    sources = { "nvim_diagnostic" },
+    sections = { "error", "warn" },
+    symbols = { 
+        error = " ", 
+        warn = " " },
+    colored = false,
+    update_in_insert = false,
+    always_visible = false,
+}
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " "},
+    cond = hide_in_width
+}
+
+local filetype = {
+    "filetype",
+    incons_enabled = true
+}
+
+local inactive_path = {
+    "filename",
+    file_status = true,
+    path = 2
+}
+
+local spaces = function()
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        disable_filetypes = {},
+        always_divide_middle = true,
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", diff },
+        lualine_c = { diagnostics },
+        lualine_x = { filetype },
+        lualine_y = { spaces, "encoding", "fileformat" },
+        lualine_z = { "location", "progress" },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { inactive_path },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location", "progress" },
+    }
+
+})
--- nvim/config/packer.lua
+++ nvim/config/packer.lua
@@ -41,6 +41,10 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
     use "theniceboy/nvim-deus"
+    
+    use { 'nvim-lualine/lualine.nvim', requires = { 
+          'kyazdani42/nvim-web-devicons', opt = true }
+    }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
