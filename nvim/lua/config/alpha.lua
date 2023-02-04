26c08c9563fd5d73ca28c238bdf01b59234ac984
lukstep <lukaszstepien02@gmail.com>
2023-02-04 10:25:28 +0100

    Show nvim version on welcom buffer

--- nvim/lua/config/alpha.lua
+++ nvim/lua/config/alpha.lua
@@ -16,12 +16,19 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	-- dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
+
+local function footer()
+    local nvim_version = vim.version()
+    return ' ' .. nvim_version.major .. '.' .. nvim_version.minor .. '.' .. nvim_version.patch
+end
+
+dashboard.section.footer.val = footer()
 
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
