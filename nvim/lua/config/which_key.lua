bbe2582dca5e9619d2348ab5fc0efebf8ee6910a
lukstep <lukaszstepien02@gmail.com>
2023-04-23 10:42:56 +0200

    Add which key group icon

--- nvim/lua/config/which_key.lua
+++ nvim/lua/config/which_key.lua
@@ -10,6 +10,11 @@ end
 
 wk.setup({
     ignore_missing = true,
+    icons = {
+        breadcrumb = "»",
+        separator = "➜",
+        group = "",
+    },
 })
 
 wk.register({
@@ -20,21 +25,21 @@ wk.register({
         --   keymap('n', ('<Leader>%s'):format(i), ('<Plug>(cokeline-focus-%s)'):format(i))
         -- end
         d = {
-            name = "+Diagnostic",
+            name = " Diagnostic",
             n = { ":lua vim.diagnostic.goto_next()<cr>", "Next diagnostic" },
             p = { ":lua vim.diagnostic.goto_prev()<cr>", "Prev diagnostic" },
             t = { "<cmd>Telescope diagnostics<cr>", "List" },
             l = { "<cmd>Lspsaga show_line_diagnostics<CR>", "Peek diagnostic" },
         },
         c = {
-            name = "+Copy",
+            name = "󰆏 Copy",
             n = { "<cmd>FileName<cr>", "Copy file name" },
             p = { "<cmd>FileRelativePath<cr>", "Copy file path" },
             P = { "<cmd>FileAbsolutePath<cr>", "Copy file full path" }
         },
         e = { "<cmd>NvimTreeToggle<cr>", "File explorer" },
         f = {
-            name = "+Find",
+            name = " Find",
             b = { "<cmd>Telescope buffers<cr>", "Find buffer" },
             B = { "<cmd>lua TelescopeFindWordInBuild(vim.fn.expand('<cword>'))<cr>", "Find in build" },
             g = { "<cmd>lua TelescopeFindWordWitoutIgnore(vim.fn.expand('<cword>'))<cr>", "Find include ignore" },
@@ -48,7 +53,7 @@ wk.register({
             t = { "<cmd>lua TelescopeFindWordInFolder(vim.fn.expand('<cword>'), 'test*')<cr>", "Find in test" }
         },
         g = {
-            name = "+Git",
+            name = " Git",
             s = { "<cmd>Telescope git_status<cr>", "Git status" },
             c = {
                 name = "+Git conflicts",
@@ -63,7 +68,7 @@ wk.register({
             v = { "<cmd>DiffviewFileHistory %<cr>", "Show current file diff"}
         },
         l = {
-            name = "+LSP",
+            name = " LSP",
             a = { "<cmd>Lspsaga code_action<cr>", "Code action" },
             d = { ":lua vim.lsp.buf.definition()<cr>", "Go to definition" },
             D = { ":lua vim.lsp.buf.declaration()<cr>", "Go to declaration" },
@@ -75,9 +80,10 @@ wk.register({
             -- vim.keymap.set("n", "pd", "<cmd>Lspsaga peek_definition<CR>", {buffer = 0})
             s = { "<cmd>Lspsaga lsp_finder<CR>", "LSP finder" },
             t = { ":lua vim.lsp.buf.type_definition()<cr>", "Type definition" },
+            w = { ":%s/\\s\\+$//e<cr>", "Trim trailing whitespace" },
         },
         t = {
-            name = "+Terminal",
+            name = " Terminal",
             g = { "<cmd>lua _TIG_TOGGLE()<cr>", "Toggle Tig terminal" },
             p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Toggle Python terminal" },
             t = { "<cmd>ToggleTerm()<cr>", "Toggle Terminal" }
