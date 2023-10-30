9d36e0dae89eb76005fee933eafc8b2878e3f314
lukstep <lukaszstepien02@gmail.com>
2023-10-30 17:49:13 +0100

    Alpha config refactor

--- nvim/lua/config/alpha.lua
+++ nvim/lua/config/alpha.lua
@@ -1,6 +1,5 @@
-local status_ok, alpha = pcall(require, "alpha")
-if not status_ok then
-    vim.notify("Alpha can't be found", "error", { title = "Start-up" })
+local alpha = load_plugin("alpha")
+if not alpha then
     return
 end
 
@@ -14,28 +13,27 @@ dashboard.section.header.val = {
     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-    dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("g", "  Git status", ":Telescope git_status<cr>"),
-    dashboard.button("b", "  Bookmarks", ":Telescope harpoon marks<cr>"),
+    dashboard.button("f", "󰱼  Find file", ":Telescope find_files <CR>"),
+    dashboard.button("e", "󰈔  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("g", "󰊢  Git status", ":Telescope git_status<cr>"),
+    dashboard.button("b", "󰃀  Bookmarks", ":Telescope harpoon marks<cr>"),
     -- dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-    dashboard.button("t", "  Find text", ":Telescope live_grep_args <CR>"),
+    dashboard.button("r", "󱋡  Recently used files", ":Telescope oldfiles <CR>"),
+    dashboard.button("t", "󱩾  Find text", ":Telescope live_grep_args <CR>"),
     dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
 }
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.opts.opts.noautocmd = true
 
 local function footer()
     local nvim_version = vim.version()
     return ' ' .. nvim_version.major .. '.' .. nvim_version.minor .. '.' .. nvim_version.patch
 end
-
 dashboard.section.footer.val = footer()
 
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
-dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
