d97d2ef0f84a313ea104a2b0ce1bb0fcc46081f1
lukstep <lukaszstepien02@gmail.com>
2023-11-03 19:17:07 +0100

    Move editing support plugins to one file

--- nvim/lua/config/editing_support.lua
+++ nvim/lua/config/editing_support.lua
@@ -1,6 +1,27 @@
-local status_ok, comment = pcall(require, "Comment")
-if not status_ok then
-    vim.notify("Comment not found!", "error", { title = "Start-up" })
+local harpoon = load_plugin("harpoon")
+if not harpoon then
+    return
+end
+
+harpoon.setup({
+
+})
+
+
+local indent_blankline = load_plugin("ibl")
+if not indent_blankline then
+    return
+end
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+indent_blankline.setup()
+
+
+local comment = load_plugin("Comment")
+if not comment then
     return
 end
 
@@ -47,3 +68,20 @@ comment.setup({
     ---Function to call after (un)comment
     post_hook = nil,
 })
+
+
+local autopairs = load_plugin("nvim-autopairs")
+if not autopairs then
+    return
+end
+
+autopairs.setup({
+    check_ts = true,
+    ts_config = {
+        lua = { 'string' }, -- it will not add a pair on that treesitter node
+        javascript = { 'template_string' },
+        java = false,     -- don't check treesitter on java
+    },
+})
+
+local ts_conds = require('nvim-autopairs.ts-conds')
--- nvim/lua/config/harpoon.lua
+++
@@ -1,9 +0,0 @@
-local present, harpoon = pcall(require, "harpoon")
-if not present then
-    vim.notify("Harpoon not found!", "error", { title = "Start-up" })
-    return
-end
-
-harpoon.setup({
-
-})
